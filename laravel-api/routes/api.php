0f5535875859237d6916aa32b3e04829fcd15c3d
thommie-wallin <thommie.wallin@chasacademy.se>
2021-05-02 15:47:09 +0200

    create authentication with AuthController and middleware group in api.php

--- laravel-api/routes/api.php
+++ laravel-api/routes/api.php
@@ -2,8 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\UserListController;
 use App\Http\Controllers\RecipeController;
+use App\Http\Controllers\AuthController;
 
 
 /*
@@ -25,11 +27,20 @@ Route::get('/userlist', [UserListController::class, 'index']);
 Route::get('/userlist/{id}', [UserListController::class, 'show']);
 Route::get('/userlist/search/{title}', [UserListController::class, 'search']);
 Route::post('/userlist', [UserListController::class, 'store']);
-Route::post('/userlist/{id}', [UserListController::class, 'update']);
+Route::put('/userlist/{id}', [UserListController::class, 'update']);
 Route::delete('/userlist/{id}', [UserListController::class, 'destroy']);
 
 Route::get('/recipe', [RecipeController::class, 'index']);
 Route::get('/recipe/{id}', [RecipeController::class, 'show']);
 Route::post('/recipe', [RecipeController::class, 'store']);
-Route::post('/recipe/{id}', [RecipeController::class, 'update']);
+Route::put('/recipe/{id}', [RecipeController::class, 'update']);
 Route::delete('/recipe/{id}', [RecipeController::class, 'destroy']);
+
+// Public routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Protected routes
+Route::group(['middleware' => ['auth:sanctum']], function() {
+  Route::post('/logout', [AuthController::class, 'logout']);
+});
